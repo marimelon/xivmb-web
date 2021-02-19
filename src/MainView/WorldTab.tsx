5d4913f5ab995ab6b86163552d965801546a2a04
marimelon <29140293+marimelon@users.noreply.github.com>
2021-02-19 17:36:33 +0900

    add WorldTabDoropDown

--- src/MainView/WorldTab.tsx
+++ src/MainView/WorldTab.tsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import style from './WorldTab.module.scss'
+import Select from '@material-ui/core/Select'
+import MenuItem from '@material-ui/core/MenuItem'
+import { makeStyles } from '@material-ui/core/styles'
+import useDimensions from 'react-cool-dimensions'
+
+const worldlist = [
+  'Elemental',
+  'Atomos',
+  'Aegis',
+  'Carbuncle',
+  'Garuda',
+  'Gungnir',
+  'Kujata',
+  'Ramuh',
+  'Tonberry',
+  'Typhon',
+  'Unicorn',
+]
 
 interface Props {
   currentTabType: string
   onClick: (item: string) => void
 }
 
-const WorldTab = React.memo(({ currentTabType, onClick }: Props) => {
-  const worldlist = [
-    'Elemental',
-    'Atomos',
-    'Aegis',
-    'Carbuncle',
-    'Garuda',
-    'Gungnir',
-    'Kujata',
-    'Ramuh',
-    'Tonberry',
-    'Typhon',
-    'Unicorn',
-  ]
+const WorldTabNomal = React.memo(({ currentTabType, onClick }: Props) => {
   return (
     <nav>
       <ul className={style.WorldTab}>
@@ -37,5 +42,73 @@ const WorldTab = React.memo(({ currentTabType, onClick }: Props) => {
     </nav>
   )
 })
+
+const useStyles = makeStyles({
+  icon: {
+    color: '#CCCCCC',
+  },
+  select: {
+    '& ul': {
+      backgroundColor: '#192734',
+    },
+    '& li': {
+      color: '#CCCCCC',
+      fontSize: 12,
+    },
+    '& li:hover': {
+      color: 'rgb(79, 180, 243)',
+    },
+  },
+})
+
+const WorldTabDoropDown = React.memo(({ currentTabType, onClick }: Props) => {
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    onClick(event.target.value as string)
+  }
+
+  const classes = useStyles()
+
+  return (
+    <div className={style.WorldTab}>
+      <Select
+        disableUnderline
+        value={currentTabType}
+        onChange={handleChange}
+        className={style.CompactWorldTab}
+        classes={{ icon: classes.icon }}
+        MenuProps={{ classes: { paper: classes.select } }}>
+        {worldlist.map(item => (
+          <MenuItem key={item} value={item}>
+            {item}
+          </MenuItem>
+        ))}
+      </Select>
+    </div>
+  )
+})
+
+const WorldTab = (props: Props) => {
+  const { ref, currentBreakpoint } = useDimensions<HTMLDivElement>({
+    // COMPACT : 0px - 759px , FULL : 760px ~
+    breakpoints: { COMPACT: 0, FULL: 760 },
+    updateOnBreakpointChange: true,
+  })
+  return (
+    <div ref={ref}>
+      {currentBreakpoint === 'COMPACT' && (
+        <WorldTabDoropDown
+          currentTabType={props.currentTabType}
+          onClick={props.onClick}
+        />
+      )}
+      {currentBreakpoint === 'FULL' && (
+        <WorldTabNomal
+          currentTabType={props.currentTabType}
+          onClick={props.onClick}
+        />
+      )}
+    </div>
+  )
+}
 
 export default WorldTab
