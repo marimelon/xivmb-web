116d4e5bd2b95e43e30a5a2efccc959c564c6d44
marimelon <29140293+marimelon@users.noreply.github.com>
2021-08-16 20:12:05 +0900

    Redirect to the last item viewed page

--- src/AppRoute.tsx
+++ src/AppRoute.tsx
@@ -1,28 +1,63 @@
-import React, { Component, lazy, Suspense } from 'react'
+import React, { Component, lazy, Suspense, useEffect, useState } from 'react'
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 import Auth from './Auth'
+import firebase from './Common/firebase'
 import LoadingPage from './Common/LoadingPage'
-
 
 const SignIn = lazy(() => import('./Signin'))
 const App = lazy(() => import('./App'))
 
 const Page404 = () => <div>Page Not Found.</div>
+const Redirect2LastItemPage: React.FC = () => {
+  const [goto, setGoto] = useState<number>()
 
+  console.log('Page1')
+
+  useEffect(() => {
+    var unsubscribe: (() => void) | undefined = undefined
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        unsubscribe = firebase
+          .firestore()
+          .collection('user_histories')
+          .doc(user.uid)
+          .onSnapshot(doc => {
+            if (doc.exists) {
+              const items = doc.get('history') as number[]
+              console.log(items)
+              if (items.length > 0) {
+                setGoto(items[items.length - 1])
+                return
+              }
+            }
+            setGoto(2)
+          })
+      }
+    })
+    return () => {
+      unsubscribe && unsubscribe()
+    }
+  }, [])
+
+  if (!goto) {
+    return <LoadingPage />
+  }
+
+  return <Redirect to={`/${goto}`} />
+}
 class AppRoute extends Component {
   render() {
     return (
       <BrowserRouter>
         <Suspense fallback={<LoadingPage />}>
           <Switch>
-            <Redirect exact from="/" to="/2" />
             <Redirect exact from="/view" to="/view/2" />
             <Route path="/signin" component={SignIn} />
-            <Route path="/:itemid(\d+)">
-              <Auth>
-                <Route path="/:itemid(\d+)" component={App} />
-              </Auth>
-            </Route>
+            <Auth>
+              <Route exact path="/" component={Redirect2LastItemPage} />
+              <Route path="/:itemid(\d+)" component={App} />
+            </Auth>
             <Route component={Page404} />
           </Switch>
         </Suspense>
