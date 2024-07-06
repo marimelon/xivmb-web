3e13ff753f9885f84a345a134f1764b76e49da68
marimelon <29140293+marimelon@users.noreply.github.com>
2024-07-06 04:33:17 +0000

    Do not refresh the history list until rerendering

--- src/components/HistoryList/index.tsx
+++ src/components/HistoryList/index.tsx
@@ -2,15 +2,18 @@ import { Card, List, ListItem } from '@yamada-ui/react'
 
 import { Item } from '../../types/item'
 import { SidebarItem } from '../SidebarItem'
+import { useState } from 'react'
 
 type Props = {
   items: Item[]
 }
 
 export const HistoryList = ({ items }: Props) => {
+  const [_items] = useState(items) // 再表示されるまで更新しない
+
   return (
     <List>
-      {items.map((item) => (
+      {_items.map((item) => (
         <ListItem key={item.id}>
           <Card height={10} variant="outline">
             <SidebarItem item={item} />
