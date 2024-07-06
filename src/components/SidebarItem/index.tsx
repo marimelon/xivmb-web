1238198ac7d1ae793f4c98ee9a706785c7abe9c4
marimelon <29140293+marimelon@users.noreply.github.com>
2024-07-06 13:57:13 +0000

    active item

--- src/components/SidebarItem/index.tsx
+++ src/components/SidebarItem/index.tsx
@@ -1,6 +1,6 @@
 import { useCallback } from 'react'
 
-import { useNavigate, useSearch } from '@tanstack/react-router'
+import { useNavigate, useParams, useSearch } from '@tanstack/react-router'
 import { HStack, Text } from '@yamada-ui/react'
 
 import { Item } from '../../types/item'
@@ -13,7 +13,9 @@ type Props = {
 
 export const SidebarItem = ({ item, onClick }: Props) => {
   const navigate = useNavigate()
+  const { itemid } = useParams({ from: '/$itemid/' })
   const { dc } = useSearch({ from: '/$itemid/' })
+
   const defaultHandleClick = useCallback(() => {
     navigate({
       to: '/$itemid',
@@ -22,20 +24,27 @@ export const SidebarItem = ({ item, onClick }: Props) => {
     })
   }, [dc, item.id, navigate])
 
-  const handleClick: React.MouseEventHandler<HTMLDivElement> =
-    onClick ?? defaultHandleClick
+  const currentItem = itemid === String(item.id)
+  const handleClick: React.MouseEventHandler<HTMLDivElement> | undefined =
+    currentItem ? undefined : onClick ?? defaultHandleClick
 
   return (
     <HStack
       height="100%"
       width="100%"
-      cursor="pointer"
+      cursor={currentItem ? 'default' : 'pointer'}
       gap={1}
-      _hover={{
-        borderColor: '#8bb6e5',
-        borderWidth: 1,
-        boxShadow: '0rem 0rem 1rem 0rem rgba(137, 193, 202, 0.41)',
-      }}
+      _hover={
+        currentItem
+          ? {}
+          : {
+              borderColor: '#8bb6e5',
+              borderWidth: 1,
+              boxShadow: '0rem 0rem 1rem 0rem rgba(137, 193, 202, 0.41)',
+            }
+      }
+      color={currentItem ? 'primary' : ''}
+      backgroundColor={currentItem ? 'whiteAlpha.100' : ''}
       onClick={handleClick}
     >
       <ItemIcon item={item} />
